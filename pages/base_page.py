34e268e289701766ba499cc3bc2b4cab46ed8cd1
Eugene Pestov <forourlife2010@yandex.ru>
2022-09-28 17:40:03 +0300

    Add some new test. Move several function from MainPage to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -11,6 +12,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -33,3 +38,6 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+    
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
